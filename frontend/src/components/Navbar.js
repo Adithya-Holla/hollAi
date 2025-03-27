2a1198b29bea5dc37427c965a3e6f56f0ba58054
Adithya-Holla <145368129+Adithya-Holla@users.noreply.github.com>
2025-03-27 11:01:09 +0530

    Fix ESLint warnings in Navbar.js by removing unused code

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,35 +4,6 @@ import '../styles/Navbar.css';
 import ThemeToggle from './ThemeToggle';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
-// RouterLink component for use within Router context
-const RouterLink = ({ to, children, className, onClick }) => {
-  return (
-    <Link to={to} onClick={onClick} className={className}>
-      {children}
-    </Link>
-  );
-};
-
-// RegularLink component for use outside Router context
-const RegularLink = ({ to, children, className, onClick }) => {
-  return (
-    <a href={to} onClick={onClick} className={className}>
-      {children}
-    </a>
-  );
-};
-
-// This function checks if we're in a router context
-const checkRouterContext = () => {
-  try {
-    // This will throw an error if we're not in a router context
-    require('react-router-dom').useLocation();
-    return true;
-  } catch (error) {
-    return false;
-  }
-};
-
 function Navbar({ toggleTheme }) {
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
